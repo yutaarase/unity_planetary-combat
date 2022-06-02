0373d35129d5b3ca4f4f2d1afdff62803e259ff4
yutaarase <arase210601@eggnet.ac.jp>
2022-06-02 12:40:03 +0900

    20220602

---
+++ planetary-combat/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Mirror.PlanetaryCombat
+{
+	[RequireComponent(typeof(Rigidbody))]
+	public class GravityBody : MonoBehaviour
+	{
+
+		GravityAttractor planet;
+		Rigidbody rigidbody;
+
+		void Awake()
+		{
+			planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
+			rigidbody = GetComponent<Rigidbody>();
+
+			// Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
+			rigidbody.useGravity = false;
+			rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+		}
+
+		void FixedUpdate()
+		{
+			// Allow this body to be influenced by planet's gravity
+			planet.Attract(rigidbody);
+		}
+
+		[ServerCallback]
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.tag == "Planet")
+			{
+				planet = other.gameObject.GetComponent<GravityAttractor>();
+			}
+		}
+	}
+}
